ffb14f03cd61c7a8ff8065c7bee93a7d70a1a9c6
hichem2h <hichem2h@gmail.com>
2019-09-25 00:10:33 +0100

    Add non-tested S3 uploader

--- src/config.js
+++ src/config.js
@@ -5,3 +5,6 @@ export const jwtOptions = {
     expiresIn:  "10 days",
     algorithm:  "HS256"
 };
+
+export const AWS_ACCESS_KEY = '1234';
+export const AWS_SECRET_ACCESS_KEY = '1234';
--- src/graphql/anomalies/imageUtils.js
+++ src/graphql/anomalies/imageUtils.js
@@ -1,7 +1,13 @@
 import fs from 'fs'
+import AWS from 'aws-sdk'
 import shortid from 'shortid'
 import { ValidationError } from 'apollo-server';
+import { AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY } from '../../config'
 
+const s3 = new AWS.S3({
+  accessKeyId: AWS_ACCESS_KEY,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY
+});
 
 const storeFs = async (image) => {
   try {
@@ -28,8 +34,26 @@ const storeFs = async (image) => {
     if (error.name != 'PayloadTooLargeError') throw error
     throw new ValidationError(error.message);
   }
-
 }
+
+const uploadFileToS3 = async (image) => {
+  const { createReadStream, filename } = await image;
+  const stream = createReadStream()
+  const id = shortid.generate()
+  const path = `uploads/${id}-${filename}`
+
+     const params = {
+         Bucket: 'testBucket',
+         Key: path,
+         Body: JSON.stringify(stream, null, 2)
+     };
+
+     s3.upload(params, function(s3Err, data) {
+         if (s3Err) throw s3Err
+         
+         return data.Location
+     });
+};
 
 export const processImages = async (images) => {
   const links = await Promise.all(images.map(storeFs))
